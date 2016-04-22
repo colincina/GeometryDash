183e08fb9838a7cd7c3adc6cf886a71725e52163
morechicken <colincina2@gmail.com>
2016-04-22 14:20:47 +0200

    Changements test yolo
    Questque c'est !?!???!?! Salut Martin

---
+++ Geometry Dash Project/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	
+	int bar; 
+	int foo; 
+	
+	Test (){
+		bar = 3; 
+		foo = 2; 
+	}
+
+	public static void main (String args[]){
+		
+		Test t = new Test(); 
+		System.out.println(t.foo);
+		System.out.println(t.bar);
+	}
+}
