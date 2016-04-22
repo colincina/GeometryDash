6d194778b5f94aa7398efce04200c622be4ffaa9
MartinJuon <mjuon95@gmail.com>
2016-04-22 23:00:53 +0200

    Projekt Struktur

---
+++ Geometry Dash Project/src/Projekt_Struktur.java
@@ -0,0 +1,16 @@
+
+public class Projekt_Struktur {
+// Ciao monsieur, j'ai ecris une petit structure pour notre projet. 
+// Chaque "-->" est responsable pour un action, si on appyue un boutton. P.e Button Settings
+	
+// Startmenu --> Settings und Game start
+	
+	//Settings --> Configure Dash und Sound on off
+		//Configure Dash --> Color und Performance
+	
+	//Game start --> Spiel beginnt 
+		//Spiel läuft und Welt muss generiert werden
+		//Spiel ende Gameover-Bildschirm --> Restart oder Exit
+			// Exit --> Return to Startmenu
+			
+}
